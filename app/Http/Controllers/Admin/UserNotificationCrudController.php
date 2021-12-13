ac15e44ba4c2a88c4abcb49c4e55b95f7ea0d223
shaheen2013 - AfzalSabbir <coredeveloper.2013@gmail.com>
2021-12-13 21:57:53 +0600

    User Notifications

---
+++ app/Http/Controllers/Admin/UserNotificationCrudController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\UserNotificationRequest;
+use Backpack\CRUD\app\Http\Controllers\CrudController;
+use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+use Carbon\Carbon;
+
+/**
+ * Class UserNotificationCrudController
+ * @package App\Http\Controllers\Admin
+ * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
+ */
+class UserNotificationCrudController extends CrudController
+{
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+
+    /**
+     * Configure the CrudPanel object. Apply settings to all operations.
+     *
+     * @return void
+     */
+    public function setup()
+    {
+        CRUD::setModel(\App\Models\UserNotification::class);
+        CRUD::setRoute(config('backpack.base.route_prefix') . '/user-notification');
+        CRUD::setEntityNameStrings('user notification', 'user notifications');
+    }
+
+    /**
+     * Define what happens when the List operation is loaded.
+     *
+     * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
+     * @return void
+     */
+    protected function setupListOperation()
+    {
+        CRUD::column('id');
+        CRUD::column('client_name');
+        CRUD::column('birthday');
+        //CRUD::column('age');
+        CRUD::column('marital_status');
+        /*CRUD::column('created_at');
+        CRUD::column('updated_at');*/
+
+        /**
+         * Columns can be defined using the fluent syntax or array syntax:
+         * - CRUD::column('price')->type('number');
+         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']);
+         */
+    }
+
+    /**
+     * Define what happens when the Create operation is loaded.
+     *
+     * @see https://backpackforlaravel.com/docs/crud-operation-create
+     * @return void
+     */
+    protected function setupCreateOperation()
+    {
+        CRUD::setValidation(UserNotificationRequest::class);
+
+        //CRUD::field('id');
+        CRUD::field('client_name');
+        CRUD::field('birthday')->type('date');
+        //CRUD::field('age')->type('number');
+        CRUD::addField([
+            'name'        => 'marital_status',
+            'type'        => 'select_from_array',
+            'options'     => ['single', 'married', 'divorced', 'widowed'],
+            'allows_null' => false,
+            'default'     => 'one',
+        ]);
+        /*CRUD::field('created_at');
+        CRUD::field('updated_at');*/
+
+        /**
+         * Fields can be defined using the fluent syntax or array syntax:
+         * - CRUD::field('price')->type('number');
+         * - CRUD::addField(['name' => 'price', 'type' => 'number']));
+         */
+    }
+
+    /**
+     * Define what happens when the Update operation is loaded.
+     *
+     * @see https://backpackforlaravel.com/docs/crud-operation-update
+     * @return void
+     */
+    protected function setupUpdateOperation()
+    {
+        $this->setupCreateOperation();
+    }
+
+    /**
+     * Define what happens when the Update operation is loaded.
+     *
+     * @see https://backpackforlaravel.com/docs/crud-operation-update
+     * @return void
+     */
+    protected function setupShowOperation()
+    {
+        $this->setupCreateOperation();
+        CRUD::addColumn([
+            'name'     => 'output',
+            'label'    => 'Output',
+            'type'     => 'closure',
+            'function' => function ($entry) {
+                $age = Carbon::parse($entry->birthday)->diff(Carbon::now())->format('%y');
+                return "Hello, my name is {$entry->client_name}. I am {$age} years old. I am {$entry->marital_status}.";
+            },
+        ]);
+    }
+}
---
+++ database/migrations/2021_12_13_153452_create_user_notifications_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_notifications', function (Blueprint $table) {
+            $table->id();
+            $table->string('client_name', 191);
+            $table->date('birthday');
+            $table->enum('marital_status', ['single', 'married', 'divorced', 'widowed']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_notifications');
+    }
+}
